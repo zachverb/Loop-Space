18bed4837b9b7678db5bf16dae8046564fadf02e
zachverb <zachylax@gmail.com>
2015-04-25 13:34:44 -0700

    in the middle of updating download. Still not working 100%

---
+++ app/src/main/java/com/capstone/zacharyverbeck/audiorecordtest/API/S3API.java
@@ -0,0 +1,19 @@
+package com.capstone.zacharyverbeck.audiorecordtest.API;
+
+import retrofit.Callback;
+import retrofit.client.Response;
+import retrofit.http.GET;
+import retrofit.http.Path;
+import retrofit.http.Streaming;
+
+/**
+ * Created by zacharyverbeck on 4/25/15.
+ */
+public interface S3API {
+    @GET("/loops/{filename}")
+    @Streaming
+    void getLoop(
+            @Path("filename") String filename,
+            Callback<Response> callback
+    );
+}
--- app/src/main/java/com/capstone/zacharyverbeck/audiorecordtest/UI/MainActivity.java
+++ app/src/main/java/com/capstone/zacharyverbeck/audiorecordtest/UI/MainActivity.java
@@ -15,10 +15,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
+import com.capstone.zacharyverbeck.audiorecordtest.API.S3API;
 import com.capstone.zacharyverbeck.audiorecordtest.API.ServerAPI;
 import com.capstone.zacharyverbeck.audiorecordtest.Buttons.LoopButton;
-import com.capstone.zacharyverbeck.audiorecordtest.Models.Loop;
 import com.capstone.zacharyverbeck.audiorecordtest.Models.Endpoint;
+import com.capstone.zacharyverbeck.audiorecordtest.Models.Loop;
 import com.capstone.zacharyverbeck.audiorecordtest.R;
 
 import java.io.BufferedInputStream;
@@ -67,6 +68,8 @@ public class MainActivity extends Activity {
 
     public ServerAPI service;
 
+    public S3API s3Service;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -98,6 +101,45 @@ public class MainActivity extends Activity {
         mRightLayout = (LinearLayout)findViewById(R.id.rightLayout);
         mLoops = new Loop[6];
         addButton();
+        addButton();
+        RestAdapter restAdapter = new RestAdapter.Builder()
+                .setEndpoint("https://s3-us-west-2.amazonaws.com/loopspace/")
+                .build();
+        s3Service = restAdapter.create(S3API.class);
+
+        s3Service.getLoop("cf0271b3472f1d0cf18e-test0.pcm",
+                new Callback<Response>() {
+                    @Override
+                    public void success(Response result, Response response) {
+                        File file = new File(Environment.getExternalStorageDirectory(), "cf0271b3472f1d0cf18e-test0.pcm");
+                        try {
+                            InputStream inputStream = response.getBody().in();
+                            BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+                            DataInputStream dataInputStream = new DataInputStream(bufferedInputStream);
+
+                            OutputStream outStream = new FileOutputStream(file);
+                            byte[] buffer = new byte[8 * 1024];
+
+                            while (dataInputStream.available() > 0) {
+                                outStream.write(buffer, 0, inputStream.read(buffer));
+                            }
+
+                            outStream.close();
+                            dataInputStream.close();
+                            mLoops[0].setFilePath(file.getAbsolutePath());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+
+                    @Override
+                    public void failure(RetrofitError retrofitError) {
+                        Log.d(TAG, "FAK");
+                        retrofitError.printStackTrace();
+                    }
+                });
+
         mPlayButton = (Button) findViewById(R.id.playButton);
         mPlayButton.setOnClickListener(playBackOnClickListener);
 
@@ -142,7 +184,7 @@ public class MainActivity extends Activity {
             Log.d(TAG, "STOP REC");
             recording = false;
             addButton();
-            TypedFile soundFile = new TypedFile("audio/x-wav;codec=pcm;rate=16000", new File(mLoops[v.getId()].getFilePath()));
+            TypedFile soundFile = new TypedFile("audio/x-wav;codec=pcm;rate=44100", new File(mLoops[v.getId()].getFilePath()));
             Log.d("Spacers", "uploadin dat sheeit");
             service.upload(soundFile, new Callback<Endpoint>() {
                 @Override
