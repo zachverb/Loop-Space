617db778608aa61c4c210f977b04c541446e9c5c
zachverb <zachylax@gmail.com>
2015-05-12 16:55:24 -0700

    Using Ney's style guides and whatnot.

--- app/src/main/java/com/capstone/zacharyverbeck/audiorecordtest/UI/TrackListActivity.java
+++ app/src/main/java/com/capstone/zacharyverbeck/audiorecordtest/UI/TrackListActivity.java
@@ -1,17 +1,23 @@
 package com.capstone.zacharyverbeck.audiorecordtest.UI;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.DisplayMetrics;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import com.capstone.zacharyverbeck.audiorecordtest.API.ServerAPI;
 import com.capstone.zacharyverbeck.audiorecordtest.Java.TrackListAdapter;
@@ -19,6 +25,8 @@ import com.capstone.zacharyverbeck.audiorecordtest.Models.Data;
 import com.capstone.zacharyverbeck.audiorecordtest.Models.Track;
 import com.capstone.zacharyverbeck.audiorecordtest.R;
 import com.melnykov.fab.FloatingActionButton;
+import com.rey.material.app.Dialog;
+import com.rey.material.widget.EditText;
 
 import java.util.List;
 
@@ -101,50 +109,89 @@ public class TrackListActivity extends ActionBarActivity {
             @Override
             public void onClick(View v) {
                 Log.d(TAG, "YO");
-                service.newTrack(new Track("YO") , new Callback<Data>() {
+
+
+                final Dialog mDialog = new Dialog(TrackListActivity.this, R.style.Material_App_Dialog_Simple);
+                final boolean[] isText = new boolean[]{false};
+
+                final EditText input = new EditText(TrackListActivity.this);
+                input.setTextColor(Color.WHITE);
+                input.addTextChangedListener(new TextWatcher() {
                     @Override
-                    public void success(Data data, Response response) {
-                        Log.d(TAG, "SUCCESS!");
-                        if(data.type == true) {
-                            Intent loopIntent = new Intent(getApplicationContext(), LoopActivity.class);
-                            loopIntent.putExtra("trackId", data.id);
-                            startActivity(loopIntent);
-                        } else {
-                            Log.d(TAG, "JK, failure");
-                        }
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                        Log.d(TAG, "Before Text Changed");
                     }
 
                     @Override
-                    public void failure(RetrofitError error) {
-                        Log.d(TAG, "failed to create track");
-                        error.printStackTrace();
+                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+                        if(s.length() == 0) {
+                            isText[0] = false;
+                        } else {
+                            isText[0] = true;
+                        }
+                        Log.d(TAG, "On Text Changed");
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable s) {
+                        Log.d(TAG, "After Text Changed");
                     }
                 });
+                FrameLayout container = new FrameLayout(TrackListActivity.this);
+                FrameLayout.LayoutParams params = new  FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+                params.leftMargin = (int) convertDpToPx(15, TrackListActivity.this);
+                params.rightMargin = (int) convertDpToPx(15, TrackListActivity.this);
+                input.setLayoutParams(params);
+                input.setSingleLine();
+                container.addView(input);
+                mDialog.title("Track Name")
+                        .positiveAction("OK")
+                        .negativeAction("CANCEL")
+                        .contentView(container)
+                        .contentMargin(50)
+                        .show();
+
+
+                mDialog.positiveActionClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (isText[0] == false) {
+                            input.setHelper("Can't be empty!");
+                        } else {
+                            service.newTrack(new Track(input.getText().toString()), new Callback<Data>() {
+                                @Override
+                                public void success(Data data, Response response) {
+                                    Log.d(TAG, "SUCCESS!");
+                                    if (data.type == true) {
+                                        Intent loopIntent = new Intent(getApplicationContext(), LoopActivity.class);
+                                        loopIntent.putExtra("trackId", data.id);
+                                        //loopIntent.putExtra("title", );
+                                        startActivity(loopIntent);
+                                    } else {
+                                        Log.d(TAG, "JK, failure");
+                                    }
+                                }
+
+                                @Override
+                                public void failure(RetrofitError error) {
+                                    Log.d(TAG, "failed to create track");
+                                    error.printStackTrace();
+                                }
+                            });
+                            mDialog.dismiss();
+                        }
+                    }
+                });
+
+                mDialog.negativeActionClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mDialog.dismiss();
+                    }
+                });
+
             }
         });
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_track_list, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     public void getTracks() {
@@ -162,5 +209,12 @@ public class TrackListActivity extends ActionBarActivity {
                 error.printStackTrace();
             }
         });
+    }
+
+    public static float convertDpToPx(float dp, Context context){
+        Resources resources = context.getResources();
+        DisplayMetrics metrics = resources.getDisplayMetrics();
+        float px = dp * (metrics.densityDpi / 160f);
+        return px;
     }
 }
